c3af7a85afd3fad4cb58f50bcc522449c85f043a
DrLuke <Lukas@Jackowski.de>
2017-05-06 11:44:51 +0200

    Add deleting selected nodes and links

--- effigy/QNodeScene.py
+++ effigy/QNodeScene.py
@@ -1,4 +1,8 @@
-from PyQt5.QtWidgets import QGraphicsScene, QUndoStack
+from PyQt5.QtWidgets import QGraphicsScene, QUndoStack, QUndoCommand
+from PyQt5.QtCore import Qt
+from effigy.QNodeSceneNode import QNodeSceneNode
+from effigy.NodeIO import NodeIO
+from effigy.NodeLink import NodeLink
 
 
 class QNodeScene(QGraphicsScene):
@@ -8,7 +12,39 @@ class QNodeScene(QGraphicsScene):
         super().__init__(*__args)
 
     def keyPressEvent(self, event):
-        if event.text() == "a":
+        if event.key() == Qt.Key_A:
             self.undostack.undo()
-        else:
+        elif event.key() == Qt.Key_D:
             self.undostack.redo()
+        elif event.key() == Qt.Key_Delete:
+            self.delSelection()
+
+    def delSelection(self):
+        self.undostack.beginMacro("Delete Stuff")
+
+        selectedItems = self.selectedItems()
+
+        links = [x for x in selectedItems if issubclass(type(x), NodeLink)]
+        nodes = [x for x in selectedItems if issubclass(type(x), QNodeSceneNode)]
+        remainder = [x for x in selectedItems if not issubclass(type(x), (QNodeSceneNode, NodeLink))]
+
+        for link in links:
+            self.undostack.push(type(link.startIO).DeleteLinkCommand(link.startIO))
+
+        for node in nodes:
+            self.undostack.push(QNodeScene.DeleteNodeCommand(node))
+
+        self.undostack.endMacro()
+
+    class DeleteNodeCommand(QUndoCommand):
+        def __init__(self, node):
+            super().__init__()
+
+            self.node = node
+            self.scene = self.node.scene()
+
+        def redo(self):
+            self.scene.removeItem(self.node)
+
+        def undo(self):
+            self.scene.addItem(self.node)
